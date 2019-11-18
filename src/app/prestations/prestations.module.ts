57f4c4d619e53e5380ff6ae004338f58df6ef5e9
FORTIN <alexis.fortin@cgi.com>
2019-11-18 16:11:55 +0100

    Add: initialize all Modules with Page Components

---
+++ src/app/prestations/prestations.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PagePrestationsComponent } from './pages/page-prestations/page-prestations.component';
+
+
+
+@NgModule({
+  declarations: [PagePrestationsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PrestationsModule { }
