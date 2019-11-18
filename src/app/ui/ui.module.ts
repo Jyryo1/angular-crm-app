585066dfe9c0766ea9ec12bb7fd668339d2c18b9
FORTIN <alexis.fortin@cgi.com>
2019-11-18 17:29:30 +0100

    Add: export UI component and use it in app Component

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CoderbaseUiModule } from '@coderbase/ui';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
@@ -10,7 +11,8 @@ import { UiComponent } from './containers/ui/ui.component';
 @NgModule({
   declarations: [UiComponent, NavComponent, HeaderComponent, FooterComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule, CoderbaseUiModule
+  ],
+  exports: [UiComponent]
 })
 export class UiModule { }
