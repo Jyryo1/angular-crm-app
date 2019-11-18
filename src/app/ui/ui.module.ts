3507e49d5f0831fbc1b18a87f2295ed6624a0783
FORTIN <alexis.fortin@cgi.com>
2019-11-18 16:50:40 +0100

    Add: UI components and auto loading Modules

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { UiComponent } from './containers/ui/ui.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UiComponent, NavComponent, HeaderComponent, FooterComponent],
   imports: [
     CommonModule
   ]
