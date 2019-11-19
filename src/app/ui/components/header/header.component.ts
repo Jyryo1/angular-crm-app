aa0187fedbdfc3ef3a2ea5f2f33c8fa76ace9aa9
FORTIN <alexis.fortin@cgi.com>
2019-11-19 10:07:31 +0100

    Add: Styles

--- src/app/ui/components/header/header.component.ts
+++ src/app/ui/components/header/header.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  public title: string;
+  constructor() {
+  }
 
   ngOnInit() {
+    this.title = 'Nikki\'s app';
   }
 
 }
