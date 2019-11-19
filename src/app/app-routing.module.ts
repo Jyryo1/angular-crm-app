4bff769db3b82042329510615beff9f08eb38599
FORTIN <alexis.fortin@cgi.com>
2019-11-19 11:46:25 +0100

    Add: routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageLoginComponent } from './login/pages/page-login/page-login.component';
 
-
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: 'login', component: PageLoginComponent },
+  { path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  { path: '**', redirectTo: '/login' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -5,13 +5,14 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
 import { UiComponent } from './containers/ui/ui.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [UiComponent, NavComponent, HeaderComponent, FooterComponent],
   imports: [
-    CommonModule, CoderbaseUiModule
+    CommonModule, CoderbaseUiModule, RouterModule
   ],
   exports: [UiComponent]
 })
