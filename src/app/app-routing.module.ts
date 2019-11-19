c9e0bbe8a7e2dc299f307beceba30e0ffaf9ebc6
FORTIN <alexis.fortin@cgi.com>
2019-11-19 11:51:56 +0100

    Add: add lazy loaded route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,9 @@ const appRoutes: Routes = [
     redirectTo: '/login',
     pathMatch: 'full'
   },
-  { path: '**', redirectTo: '/login' }
+  { path: 'prestations', loadChildren: () => import('./prestations/prestations.module').then(mod => mod.PrestationsModule) },
+  { path: 'clients', loadChildren: () => import('./clients/clients.module').then(mod => mod.ClientsModule) },
+  { path: '**', loadChildren: () => import('./page-not-found/page-not-found.module').then(mod => mod.PageNotFoundModule) }
 ];
 
 @NgModule({
