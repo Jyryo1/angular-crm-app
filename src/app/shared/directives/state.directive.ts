9188bed62683f4f02d0ee7d197d2f648635a2007
FORTIN Alexis <alexis.fortin@cgi.com>
2019-11-20 14:44:15 +0100

    Add: directive to add css class

---
+++ src/app/shared/directives/state.directive.ts
@@ -0,0 +1,26 @@
+import { Directive, Input, HostBinding, OnChanges } from '@angular/core';
+import { State } from '../enums/state.enum';
+
+@Directive({
+  selector: '[appState]'
+})
+export class StateDirective implements OnChanges {
+
+  @Input() appState: State;
+  @HostBinding('class') hostClass: string;
+
+  constructor() {
+    console.log('DEBUG Directive Created');
+  }
+
+  ngOnChanges(): void {
+    // console
+    this.hostClass = this.formatClass(this.appState);
+  }
+
+  private formatClass(state: any): string {
+    return `state-${state.normalize('NFD').replace(/[\u0300-\u036f\s]/g, '').toLowerCase()}`;
+  }
+
+
+}
