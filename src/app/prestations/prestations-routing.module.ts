c8123203b0e926e96af89e749601995f2627a1b6
FORTIN Alexis <alexis.fortin@cgi.com>
2019-11-21 15:01:54 +0100

    Add page presta add

--- src/app/prestations/prestations-routing.module.ts
+++ src/app/prestations/prestations-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PagePrestationsComponent } from './pages/page-prestations/page-prestations.component';
+import { PageAddPrestationComponent } from './pages/page-add-prestation/page-add-prestation.component';
 
 const modRoutes: Routes = [
   {
     path: '',
     component: PagePrestationsComponent,
     data: {title: 'Prestations', subtitle: 'Toutes les prestas'}
+  },
+  {
+    path: 'new',
+    component: PageAddPrestationComponent,
+    data: {title: 'Prestations', subtitle: 'Ajouter une Prestation'}
   }
 ];
 
