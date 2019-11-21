e456075ef91970911172899afaa09e0f32d4a41b
FORTIN Alexis <alexis.fortin@cgi.com>
2019-11-21 09:06:23 +0100

    Del: Remove useless Directive

--- src/app/shared/directives/client-state.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ClientStateDirective } from './client-state.directive';
-
-describe('ClientStateDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ClientStateDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,15 +4,14 @@ import { TotalPipe } from './pipes/total.pipe';
 import { TableauLightComponent } from './components/tableau-light/tableau-light.component';
 import { StateDirective } from './directives/state.directive';
 import { TableauDarkComponent } from './components/tableau-dark/tableau-dark.component';
-import { ClientStateDirective } from './directives/client-state.directive';
 
 
 
 @NgModule({
-  declarations: [TotalPipe, TableauLightComponent, StateDirective, TableauDarkComponent, ClientStateDirective],
+  declarations: [TotalPipe, TableauLightComponent, StateDirective, TableauDarkComponent],
   imports: [
     CommonModule
   ],
-  exports: [TotalPipe, TableauLightComponent, StateDirective, TableauDarkComponent, ClientStateDirective]
+  exports: [TotalPipe, TableauLightComponent, StateDirective, TableauDarkComponent]
 })
 export class SharedModule { }
