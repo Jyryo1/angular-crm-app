1a918110fdd12db0cdb3d139f1f6a13da2a99c51
FORTIN Alexis <alexis.fortin@cgi.com>
2019-11-22 09:51:04 +0100

    Fix: indentation

--- src/app/shared/models/client.ts
+++ src/app/shared/models/client.ts
@@ -2,7 +2,8 @@ import { ClientI } from '../interfaces/client-i';
 import { ClientState } from '../enums/client-state.enum';
 
 export class Client implements ClientI {
-  id: string;  name: string;
+  id: string;
+  name: string;
   email: string;
   state = ClientState.ACTIF;
 
